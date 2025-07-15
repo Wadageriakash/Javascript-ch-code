8688b336fa3d44b26a2dfc3287c8bfbb9e4d0073
Akash Wadageri <wadagireakash92@gmail.com>
2025-07-15 07:10:02 +0530

    updated for the Comparation and its typeof and its datatype

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -61,4 +61,80 @@ console.log(stringNumber); // 33
 console.log(typeof stringNumber); // string
 
 
+//**************************OPERATION********************************** */
+let value = 3
+let negValue = -value
+console.log(negValue) // -3
 
+console.log(2+2) // 4
+console.log(2-2) // 0
+console.log(2*2) // 4
+console.log(2**3) // 8
+console.log(2/3) // 0.666666
+console.log(2%3) // 2
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+console.log(str3) // hello hitesh
+
+console.log("1" + 2 + 2) // 122
+console.log("1" + 2)   // 12
+console.log(1 + 2 + "2"); // 32
+
+console.log(+true) // 1
+console.log(+"") // 0
+
+let gameCounter = 100
+++gameCounter
+console.log(gameCounter) // 101
+gameCounter++
+console.log(gameCounter); // 101
+
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+
+// ***************** COMPARASION ********************
+console.log(2>1) // true
+console.log(2>=1) // true
+console.log(2<1) // false
+console.log(2==1) // false
+console.log(2!=1) // true
+
+console.log("2" > 1) // true
+console.log("02" > 1) // true
+
+console.log(null > 0) // false
+console.log(null == 0) // false
+console.log(null >= 0) // true
+
+console.log(undefined > 0) // false
+console.log(undefined == 0) // false
+console.log(undefined >= 0) // false
+
+// === (Strict check)
+
+console.log("2"=== 2); // false
+
+
+// Primitive datatypes:
+// 7 types: String, Number, Boolean, null, undefined, symbol, BigInt
+
+
+// Reference type (It is also called Non primitive)
+// Arry, Objects, Functions
+// For the Non primitive data type will having its typeof is object
+// Javascript is a dynamic typed language
+
+const scoreValue = null;
+console.log(scoreValue); // null
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id);  // Symbol(123)
+console.log(anotherId);  // Symbol(123)
+console.log(id === anotherId); false
+
+// BigInt 
+const bigNumber = 345678901234567890n 
+console.log(typeof bigNumber) // bigint
