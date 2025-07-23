680df7133cec69858a1e04e59a8251de4f3846b2
Akash Wadageri <wadagireakash92@gmail.com>
2025-07-23 07:48:57 +0530

    Functions with Object and Array as a parameter

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -50,3 +50,45 @@ console.log(loginresult); //Akash Just logged in.
 console.log(loginUserMessage());  // undefined (If there is no default username, it will return undefined).
 console.log(loginUserMessage()); // Sam Just logged in.
 
+
+
+// Here for the three dot operator we will call the spread operator as well as rest operator
+// Here we will call the restoperator
+function calculationCartPrice(...num1){
+    return num1;
+}
+console.log(calculationCartPrice(200, 400, 600, 2000, 300, 300)) // [ 200, 400, 600, 2000, 300, 300 ]
+
+
+function calculationCartPrice2(val1, val2, ...num1){
+    return num1;
+}
+// Here val1 = 200 , val2 = 400
+console.log(calculationCartPrice2(200, 400, 600, 2000, 300, 300))  // [ 600, 2000, 300, 300 ]
+
+// Object passing as an argument into the function
+const user = {
+    username: "hitesh",
+    price: 199
+}
+function handleObject(anyObject){
+    
+    console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`);
+}
+
+handleObject(user); // Username is hitesh and price is 199
+
+handleObject({
+    username: "Sam",
+    price: 300
+}); // Username is Sam and price is 300
+
+// Array passsing as an argument into the function
+
+const newArray = [200, 300,600, 700];
+
+function returnSecondValue(getArray){
+    return getArray[1];
+}
+console.log(returnSecondValue(newArray)); // 300
+console.log(returnSecondValue([200, 300,600, 700])); // 300
